be1ce616e16cd3aae5a5dac6406ea310437e91c7
Jordan <jordanallen.2.1994@gmail.com>
2019-10-11 14:49:40 -0500

    First commit

--- index_youtube.js
+++ index_youtube.js
@@ -1,36 +1,59 @@
+let nextPage = "",
+  prevPage = "";
+
 function watchForm() {
     $('#search').on('submit', function (event) {
         event.preventDefault();
         let srch = document.getElementById("searchbar");
         let srchv = srch.value;
-        var count = 0;
+        if ($("#searchbar").val() === "") {
+            return;
+        };
 
         $('#searchbar').val('');
         $('#ytvids').empty();
 
         $.ajax({
             url: 'https://www.googleapis.com/youtube/v3/search',
-            data: { part: "snippet", key: "AIzaSyA6K2KPjp5fs0kdJEN9MrO5hxkiJMKZA_s", q: srchv, maxResults: 50 },
+            data: { part: "snippet", key: "AIzaSyA6K2KPjp5fs0kdJEN9MrO5hxkiJMKZA_s", q: srchv, maxResults: 10 },
             method: "GET",
             dataType: "json",
             success: function (responseJSON) {
                 console.log(responseJSON)
-                for (let i = 0; i < 10; i++) {
+                prevPage = responseJSON.prevPageToken;
+                nextPage = responseJSON.nextPageToken;
+                responseJSON.items.forEach(function (item) {
                     $('#ytvids').append(`<li>
-                                            <a class="img" href="https://youtube.com/watch?v=${responseJSON.items[i].id.videoId}" target="_blank">
-                                            <div> ${responseJSON.items[i].snippet.title} </div>
+                                            <a class="img" href="https://youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                                            <div> ${item.snippet.title} </div>
                                             </a>
-                                            <a class="img" href="https://youtube.com/watch?v=${responseJSON.items[i].id.videoId}" target="_blank">
-                                            <img src="${responseJSON.items[i].snippet.thumbnails.default.url}" />
+                                            <a class="img" href="https://youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                                            <img src="${item.snippet.thumbnails.default.url}" />
                                             </a>
                                         </li>`
                     );
+                })
+                let buttonsText;
+                if (!prevPage && !nextPage) {
+                    buttonsText = `<div>No more pages.</div>`
+                } else if (!prevPage) {
+                    buttonsText = `<div>
+                            <input type="submit" value="next" id="forward"/>
+                        </div>`
+
+                } else if (!nextPage) {
+                    buttonsText = `<div>
+                            <input type="submit" value="previous" id="backward"/>
+                        </div>`
+
+                } else {
+                    `<div>
+              <input type="submit" value="previous" id="backward"/>
+              <input type="submit" value="next" id="forward"/>
+          </div>`
                 }
-                $('#ytvids').append(`<div>
-                                        <input type="submit" value="Previous page" id="backward" />
-                                        <input type="submit" value="Next page" id="forward" />
-                                    </div>`
-                );
+
+                $('#ytvids').append(buttonsText);
             },
             error: function (err) {
                 $('#ytvids').html(`<li>
@@ -42,87 +65,109 @@ function watchForm() {
             event.preventDefault();
             console.log("forward")
             $('#ytvids').empty();
-            count = count + 10;
-            if (count < 41) {
-                $.ajax({
-                    url: 'https://www.googleapis.com/youtube/v3/search',
-                    data: { part: "snippet", key: "AIzaSyA6K2KPjp5fs0kdJEN9MrO5hxkiJMKZA_s", q: srchv, maxResults: 50 },
-                    method: "GET",
-                    dataType: "json",
-                    success: function (responseJSON) {
-                        console.log(responseJSON)
-                        for (let i = count; i < (count + 10); i++) {
-                            $('#ytvids').append(`<li>
-                                            <a class="img" href="https://youtube.com/watch?v=${responseJSON.items[i].id.videoId}" target="_blank">
-                                            <div> ${responseJSON.items[i].snippet.title} </div>
+            $.ajax({
+                url: 'https://www.googleapis.com/youtube/v3/search',
+                data: { part: "snippet", key: "AIzaSyA6K2KPjp5fs0kdJEN9MrO5hxkiJMKZA_s", q: srchv, maxResults: 10, pageToken: nextPage },
+                method: "GET",
+                dataType: "json",
+                success: function (responseJSON) {
+                    console.log(responseJSON)
+                    prevPage = responseJSON.prevPageToken;
+                    nextPage = responseJSON.nextPageToken;
+                    responseJSON.items.forEach(function (item) {
+                        $('#ytvids').append(`<li>
+                                            <a class="img" href="https://youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                                            <div> ${item.snippet.title} </div>
                                             </a>
-                                            <a class="img" href="https://youtube.com/watch?v=${responseJSON.items[i].id.videoId}" target="_blank">
-                                            <img src="${responseJSON.items[i].snippet.thumbnails.default.url}" />
+                                            <a class="img" href="https://youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                                            <img src="${item.snippet.thumbnails.default.url}" />
                                             </a>
                                         </li>`
-                            );
-                        }
-                        $('#ytvids').append(`<div>
-                                        <input type="submit" value="Previous page" id="backward" />
-                                        <input type="submit" value="Next page" id="forward" />
-                                    </div>`
                         );
-                    },
-                    error: function (err) {
-                        $('#ytvids').html(`<li>
+                    })
+                    let buttonsText;
+
+                    if (!prevPage && !nextPage) {
+                        buttonsText = `<div>No more pages.</div>`
+                    } else if (!prevPage) {
+                        buttonsText = `<div>
+                          <input type="submit" value="next" id="forward"/>
+                        </div>`
+
+                    } else if (!nextPage) {
+                        buttonsText = `<div>
+                            <input type="submit" value="previous" id="backward"/>
+                        </div>`
+
+                    } else {
+                        buttonsText = `<div>
+                            <input type="submit" value="previous" id="backward"/>
+                            <input type="submit" value="next" id="forward"/>
+                        </div>`
+                    }
+                    $('#ytvids').append(buttonsText);
+                },
+                error: function (err) {
+                    $('#ytvids').html(`<li>
                                         Unexpected error.
                                        </li>`);
-                    },
-                });
-            } else {
-                $('#ytvids').html(`<li>
-                                        You have reached the final page and can't go to next page.
-                                       </li>`);
-            }
+                },
+            });
         });
         $('#ytvids').on("click", "#backward", function (event) {
             event.preventDefault();
             console.log("backward")
             $('#ytvids').empty();
-            count = count - 10;
-            console.log(count)
-            if (count > -1) {
 
-                $.ajax({
-                    url: 'https://www.googleapis.com/youtube/v3/search',
-                    data: { part: "snippet", key: "AIzaSyA6K2KPjp5fs0kdJEN9MrO5hxkiJMKZA_s", q: srchv, maxResults: 50 },
-                    method: "GET",
-                    dataType: "json",
-                    success: function (responseJSON) {
-                        console.log(responseJSON)
-                        for (let i = count; i < (count + 10); i++) {
-                            $('#ytvids').append(`<li>
-                                            <a class="img" href="https://youtube.com/watch?v=${responseJSON.items[i].id.videoId}" target="_blank">
-                                            <div> ${responseJSON.items[i].snippet.title} </div>
+            $.ajax({
+                url: 'https://www.googleapis.com/youtube/v3/search',
+                data: { part: "snippet", key: "AIzaSyA6K2KPjp5fs0kdJEN9MrO5hxkiJMKZA_s", q: srchv, maxResults: 10, pageToken: prevPage },
+                method: "GET",
+                dataType: "json",
+                success: function (responseJSON) {
+                    console.log(responseJSON)
+                    prevPage = responseJSON.prevPageToken;
+                    nextPage = responseJSON.nextPageToken;
+                    responseJSON.items.forEach(function (item) {
+                        $('#ytvids').append(`<li>
+                                            <a class="img" href="https://youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                                            <div> ${item.snippet.title} </div>
                                             </a>
-                                            <a class="img" href="https://youtube.com/watch?v=${responseJSON.items[i].id.videoId}" target="_blank">
-                                            <img src="${responseJSON.items[i].snippet.thumbnails.default.url}" />
+                                            <a class="img" href="https://youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                                            <img src="${item.snippet.thumbnails.default.url}" />
                                             </a>
                                         </li>`
-                            );
-                        }
-                        $('#ytvids').append(`<div>
-                                        <input type="submit" value="Previous page" id="backward" />
-                                        <input type="submit" value="Next page" id="forward" />
-                                    </div>`
                         );
-                    },
-                    error: function (err) {
-                        $('#ytvids').html(`<li>
+                    })
+                    let buttonsText;
+                    if (!prevPage && !nextPage) {
+                        buttonsText = `<div>No more pages.</div>`
+                    } else if (!prevPage) {
+                        buttonsText = `<div>
+                            <input type="submit" value="next" id="forward"/>
+                        </div>`
+
+                    } else if (!nextPage) {
+                        buttonsText = `<div>
+                            <input type="submit" value="previous" id="backward"/>
+                        </div>`
+
+                    } else {
+                        buttonsText = `<div>
+                            <input type="submit" value="previous" id="backward"/>
+                            <input type="submit" value="next" id="forward"/>
+                        </div>`
+                    }
+
+                    $('#ytvids').append(buttonsText);
+                },
+
+                error: function (err) {
+                    $('#ytvids').html(`<li>
                                         Unexpected error.
                                        </li>`);
-                    },
-                });
-            } else {
-                $('#ytvids').html(`<li>
-                                        You are on the first page and can't go to the previous page.
-                                       </li>`);
-            }
+                },
+            });
         });
     });
 }
